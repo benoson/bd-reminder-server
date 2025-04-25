516da566fef50c44991c918c5544550a875b3b68
benoson <benoson33@gmail.com>
2025-04-25 17:42:17 +0300

    w

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ const mainLoop = () => {
     const now = new Date();
     const hour = now.getHours();
 
-    if (hour !== 17) return;
+    if (hour !== 7) return;
 
     const data = JSON.parse(fs.readFileSync(DATA_FILE, "utf8") || "[]");
 
